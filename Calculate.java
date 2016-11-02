53aed39328de5b006b58d0e9c7a191c4b581c014
vadvol <bountik@mail.ru>
2016-11-02 11:16:32 +0300

    razn Calculate

--- Calculate.java
+++ Calculate.java
@@ -1,6 +1,18 @@
 public class Calculate {
 	
+	/*public static void main(String[] arg) {
+		System.out.println("Calculate..");
+		int first = Integer.valueOf(arg[0]);
+		int second = Integer.valueOf(arg[1]);
+		int sum = first + second;
+		System.out.println("Sum "+ sum);
+	}*/
+	
 	public static void main(String[] arg) {
 		System.out.println("Calculate..");
+		int first = Integer.valueOf(arg[0]);
+		int second = Integer.valueOf(arg[1]);
+		int razn = first - second;
+		System.out.println("Raznica "+ razn);
 	}
 }
